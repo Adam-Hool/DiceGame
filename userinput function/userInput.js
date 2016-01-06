cf89275806a7b0f1de5a8899c79485b470de35f0
Kaoxyooj <locksage@hotmail.com>
2016-01-06 08:45:19 -0600

    4thcommit

--- userinput function/userInput.js
+++ userinput function/userInput.js
@@ -9,10 +9,13 @@ function userInput(){
 };
 function inputCheck(namePrompt){
         if (namePrompt.length > 0)
-        	{console.log(namePrompt)
+        	{return namePrompt;
         } else if (namePrompt.length === 0)
             {userInput();};	
 };
 
 player1 = userInput();
 player2 = userInput();
+
+console.log(player1);
+console.log(player2);
