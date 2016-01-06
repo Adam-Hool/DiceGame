cb684cfba73be4c186c022d31701ff93d12f57a3
Kaoxyooj <locksage@hotmail.com>
2016-01-05 20:03:30 -0600

    2nd commit

--- userinput function/userInput.js
+++ userinput function/userInput.js
@@ -4,14 +4,16 @@ var player1, player2;
 function userInput(){
 	var namePrompt;
 	namePrompt = prompt ("Enter your name", "");
+     inputCheck(namePrompt)
+    return namePrompt;
+};
+function inputCheck(namePrompt){
         if (namePrompt.length > 0)
         	{console.log(namePrompt)
         } else if (namePrompt.length === 0)
-            {userInput();};
+            {userInput();};	
     return namePrompt;
 };
 
 player1 = userInput();
 player2 = userInput();
-
-console.log (player1, "beats" ,player2);
