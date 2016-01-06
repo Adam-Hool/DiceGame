8420fd3365d670d214dc2849c260277db35b5ceb
Kaoxyooj <locksage@hotmail.com>
2016-01-05 19:07:15 -0600

    1st commit

---
+++ userinput function/userInput.js
@@ -0,0 +1,17 @@
+"use strict";
+var player1, player2;
+
+function userInput(){
+	var namePrompt;
+	namePrompt = prompt ("Enter your name", "");
+        if (namePrompt.length > 0)
+        	{console.log(namePrompt)
+        } else if (namePrompt.length === 0)
+            {userInput();};
+    return namePrompt;
+};
+
+player1 = userInput();
+player2 = userInput();
+
+console.log (player1, "beats" ,player2);
